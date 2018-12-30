8b2737c0b056e475f5063a7c779424e0805beeee
wu <dasheng@163.com>
2018-12-30 10:40:53 +0800

    aaa

---
+++ page/setting_page.py
@@ -0,0 +1,23 @@
+import allure
+
+from base.base_aciton import BaseAciton
+import page
+"""
+负责 设置页面相关逻辑
+"""
+class SettingPage(BaseAciton):
+    def __init__(self,driver):
+        BaseAciton.__init__(self,driver)
+
+    #退出当前登录的账号
+    @allure.step('退出账号')
+    def logout_account(self):
+        #1.滑动页面低端 才会看见退出按钮 才能找到元素 drag
+         allure.attach('退出账号', '向上滑动')
+         self.swipe_screen(1)
+        #2.点击退出按钮
+         allure.attach('退出账号', '点击退出')
+         self.click_element(page.setting_center_login_out_btn)
+        #3.点击弹出对话框确定按钮
+         allure.attach('退出账号', '点击确定')
+         self.click_element(page.setting_center_login_dialog_confirm_btn)
